85a8627aa1240721e961ed8790fd1c34224c8021
alev_ <alev_ozturk@hotmail.com>
2023-01-07 14:30:20 -0500

    revised Commit

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -1,4 +1,9 @@
 package first_package;
 
 public class FirstClass { // green color means it is in staging area
+
+
+    public static void main(String[] args) {
+        System.out.println("First Class");
+    }
 }
